2b1b177775e747cfb6a624d5ea8226f0f189dec4
stefaniacerboni <cerbonistefania@gmail.com>
2024-07-29 18:11:37 +0200

    Add failing test for findCategoryById

--- src/test/java/it/unifi/financeapp/service/CategoryServiceTest.java
+++ src/test/java/it/unifi/financeapp/service/CategoryServiceTest.java
@@ -28,13 +28,36 @@ public class CategoryServiceTest {
     class HappyCases {
         @Test
         void testAddCategory() {
-            Category category = new Category("name", "description");
+            Category category = new Category("Name", "Description");
             when(categoryRepository.save(any(Category.class))).thenReturn(category);
             Category result = categoryService.addCategory(category);
             assertNotNull(result);
             Assertions.assertEquals(category.getName(), result.getName());
             Assertions.assertEquals(category.getDescription(), result.getDescription());
             verify(categoryRepository).save(category);
+        }
+
+        @Test
+        public void testSaveExistingCategory() {
+            Category existingCategory = new Category("Name", "Description");
+            existingCategory.setId(1L); // Simulate an existing category
+            when(categoryRepository.save(any(Category.class))).thenReturn(existingCategory);
+
+            Category updatedCategory = categoryService.addCategory(existingCategory);
+
+            assertNotNull(updatedCategory);
+            Assertions.assertEquals(existingCategory.getId(), updatedCategory.getId());
+            verify(categoryRepository).save(existingCategory);
+        }
+
+        @Test
+        void testFindCategoryById() {
+            Category expectedCategory = new Category("categoryName", "description");
+            when(categoryRepository.findById(expectedCategory.getId())).thenReturn(expectedCategory);
+            Category result = categoryService.findCategoryById(expectedCategory.getId());
+            assertNotNull(result);
+            Assertions.assertEquals(expectedCategory, result);
+            verify(categoryRepository).findById(expectedCategory.getId());
         }
     }
 }
