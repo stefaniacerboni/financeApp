c15a3712c12606195bb11f65d385e71f6414b897
stefaniacerboni <cerbonistefania@gmail.com>
2024-07-29 16:52:46 +0200

    Implement Category class to pass creation test

--- src/main/java/it.unifi.financeapp/model/Category.java
+++ src/main/java/it.unifi.financeapp/model/Category.java
@@ -8,4 +8,12 @@ public class Category {
         this.name = name;
         this.description = description;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
