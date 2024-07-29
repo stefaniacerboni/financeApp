53f3fbdaacc8816ef5a4b14d21ea9982fbf4969c
stefaniacerboni <cerbonistefania@gmail.com>
2024-07-29 17:10:36 +0200

    Implement User class to pass creation test

--- src/main/java/it.unifi.financeapp/model/User.java
+++ src/main/java/it.unifi.financeapp/model/User.java
@@ -12,4 +12,45 @@ public class User {
         this.surname = surname;
         this.email = email;
     }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public User() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/test/java/it.unifi.financeapp/model/UserTest.java
+++ src/test/java/it.unifi.financeapp/model/UserTest.java
@@ -2,7 +2,11 @@ package it.unifi.financeapp.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class UserTest {
+
     @Test
     void testCreateUser(){
         String username = "Username";
@@ -10,6 +14,37 @@ public class UserTest {
         String surname = "Surname";
         String email = "Email";
         User user = new User(username, name, surname, email);
+        assertEquals(username, user.getUsername());
         assertEquals(name, user.getName());
+        assertEquals(surname, user.getSurname());
+        assertEquals(email, user.getEmail());
+    }
+
+    @Test
+    void testSetUserDetails(){
+        String username = "Username";
+        String name = "Name";
+        String surname = "Surname";
+        String email = "Email";
+        User user = new User();
+        user.setUsername(username);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setEmail(email);
+        assertEquals(username, user.getUsername());
+        assertEquals(name, user.getName());
+        assertEquals(surname, user.getSurname());
+        assertEquals(email, user.getEmail());
+    }
+
+    @Test
+    void testCreateUserWithNoNameSurname(){
+        String username = "Username";
+        String email = "Email";
+        User user = new User(username, email);
+        assertEquals(username, user.getUsername());
+        assertNull(user.getName());
+        assertNull(user.getSurname());
+        assertEquals(email, user.getEmail());
     }
 }
