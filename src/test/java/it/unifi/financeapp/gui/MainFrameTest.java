a44c3feabe07a1676d9973dff9006d2c6c0818b8
stefaniacerboni <cerbonistefania@gmail.com>
2024-08-01 23:18:22 +0200

    Incrementing code coverage by adding tests in MainFrameTest.java. Reformatting for better readability in Panels

--- src/test/java/it/unifi/financeapp/gui/MainFrameTest.java
+++ src/test/java/it/unifi/financeapp/gui/MainFrameTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -18,20 +19,31 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MainFrameTest {
-
     @Mock
     private CategoryController categoryController;
     @Mock
     private UserController userController;
     @Mock
     private ExpenseController expenseController;
+    @Mock
+    private CategoryPanel categoryPanel;
+    @Mock
+    private UserPanel userPanel;
+    @Mock
+    private ExpensePanel expensePanel;
+
+    @InjectMocks
+    private MainFrame mainFrame;
 
     private FrameFixture window;
 
     @BeforeEach
     public void setUp() {
         // Note: Ensure mocks are correctly set to return panels before creating MainFrame
-        JFrame frame = GuiActionRunner.execute(() -> new MainFrame(categoryController, userController, expenseController));
+        JFrame frame = GuiActionRunner.execute(() -> {
+            mainFrame = new MainFrame(categoryController, userController, expenseController);
+            return mainFrame;
+        });
         window = new FrameFixture(frame);
         window.show();
     }
