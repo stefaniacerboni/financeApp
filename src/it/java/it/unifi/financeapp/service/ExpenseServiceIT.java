e749979b54aa4cbe079bc01f32ee993c72c6d724
stefaniacerboni <stefania.cerboni@unifi.it>
2025-03-08 17:31:45 +0100

    Corrected Expense Service IT no unique constraint and autoincremental id

--- src/it/java/it/unifi/financeapp/service/ExpenseServiceIT.java
+++ src/it/java/it/unifi/financeapp/service/ExpenseServiceIT.java
@@ -1,31 +1,32 @@
 package it.unifi.financeapp.service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 import it.unifi.financeapp.model.Category;
 import it.unifi.financeapp.model.Expense;
 import it.unifi.financeapp.model.User;
 import it.unifi.financeapp.repository.ExpenseRepository;
 import it.unifi.financeapp.repository.ExpenseRepositoryImpl;
-
-import org.hibernate.service.spi.ServiceException;
-import org.junit.jupiter.api.*;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
 class ExpenseServiceIT {
@@ -126,23 +127,6 @@ class ExpenseServiceIT {
 		assertNull(queriedPostDelete, "The expense lingers like a bad odor even after deletion. Intriguing!");
 	}
 	
-	@Test
-	void testNewExpenseConcurrent() {
-		Expense expense = new Expense(category, user, 3.50, "2024-07-15");
-		List<Thread> threads = IntStream.range(0,  10).mapToObj( i -> new Thread(() -> {
-			try {
-				expenseService.addExpense(expense);
-			}catch (ServiceException e) {
-				e.printStackTrace();
-			}
-		}
-		))
-				.peek(t -> t.start())
-				.collect(Collectors.toList());
-		await().atMost(10, TimeUnit.SECONDS).until(() -> threads.stream().noneMatch(t-> t.isAlive()));
-		assertThat(expenseService.getAllExpenses()).containsExactly(expense);
-	}
-
 	@Test
 	void testUpdateExpense() {
 		// Create and save an initial expense
